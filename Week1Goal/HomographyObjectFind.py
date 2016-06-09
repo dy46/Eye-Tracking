2357ff6cc3e4f3b18ee2489794549e12a7499815
David Yang <dy46@duke.edu>
2016-06-09 14:32:39 -0400

    cleaned up week 1 folder

--- Week1Goal/HomographyObjectFind.py
+++ Week1Goal/HomographyObjectFind.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 import cv2
-import datamani
+import DataManipulationWeek1
 
 MIN_MATCH_COUNT = 10
 
@@ -10,7 +10,7 @@ img1 = cv2.imread('feature1.png',0)          # queryImage
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-videoData = datamani.createVideoData(open('1.txt', 'r'))
+videoData = DataManipulationWeek1.createVideoData(open('Week1.txt', 'r'))
 
 cap = cv2.VideoCapture('jony.mp4')
 framecount = 0.0;
@@ -23,7 +23,7 @@ height = 960
 # # print height
 capSize = (width,height) # this is the size of my source video
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-success = cv2.VideoWriter('adios20.mp4',fourcc,fps,capSize)
+success = cv2.VideoWriter('Week1Goal.mp4',fourcc,fps,capSize)
 
 idx = 0 
 tail = 10
@@ -75,7 +75,7 @@ while (True):
         print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
         matchesMask = None
 
-    img2, x, y, idx, tail = datamani.drawCircle(img2, framecount, videoData, idx, tail)
+    img2, x, y, idx, tail = DataManipulationWeek1.drawCircle(img2, framecount, videoData, idx, tail)
     framecount = framecount + (1.0/25.0)*1000.0
 
     draw_params = dict(matchColor = (0,255,0), # draw matches in green color
